f6a15e318a39ee9a8151e7deeab9f6ab0597499e
madse21 <madhukar.in.oxford@gmail.com>
2026-01-21 03:05:42 +0530

    Updated main.
    Changed message

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -3,7 +3,8 @@ package javaapplication4;
 public class JavaApplication4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("Github is a nice place to share code !");
     }
     
 }
+
